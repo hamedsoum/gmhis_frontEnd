081d21d8518faa223c7dc25d5db4fa25de7fb3ca
hamedsoum <hamedsoum4023@gmail.com>
2023-10-13 15:38:32 +0000

    death certificate

--- src/app/death/api/service/gmhis.death.pdf.service.ts
+++ src/app/death/api/service/gmhis.death.pdf.service.ts
@@ -12,21 +12,22 @@ export class GMHISDeathPdfService {
     buildPdf(death: GMHISDeathPartial): jsPDF {
         
         var doc = new jsPDF('p', 'mm', 'a4');
-        
-       let docs = doc.text("CERTIFICAT DE DÉCÈS", 70, 70);
+        doc = this.sharedDocPdfService.docHeader('CERTIFICAT DE DÉCÈS', 65)
         let deathDate = this.datePipe.transform(new Date(death.deathDate), 'longDate');
         let currentDate = this.datePipe.transform(new Date(), 'longDate');
 
-       doc.setFontSize(10);
-       var splitTitle = doc.splitTextToSize(`Je sousigné Docteur ${death.deathDeclaratedByName.toUpperCase()} certifie avoir constaté le décès de Mr/Mme ${death.patientFirstName.toUpperCase()} ${death.patientLastName.toUpperCase()} ;  décès survenu le ${deathDate} à son domicile.`  ,150);
-      let pre =  doc.text(splitTitle,  30,90, {lineHeightFactor: 2});
+       doc.setFontSize(14);
+       doc.setFont("arial", "normal");
+       var splitTitle = doc.splitTextToSize(`Je sousigné Docteur ${death.deathDeclaratedByName.toUpperCase()} certifie avoir constaté le décès de Mr/Mme ${death.patientFirstName.toUpperCase()} ${death.patientLastName.toUpperCase()} ;  décès survenu le ${deathDate} à son domicile.`  ,178);
+      let pre =  doc.text(splitTitle,  20,90, {lineHeightFactor: 2});
 
-       doc.text(doc.splitTextToSize('En foi de quoi , le présent certificat est établi et délivré aux parents du défunt pour servir et valoir ce que de droit.', 150), 30, 110, {lineHeightFactor: 2})
+       doc.text(doc.splitTextToSize('En foi de quoi , le présent certificat est établi et délivré aux parents du défunt pour servir et valoir ce que de droit.', 172), 20, 120, {lineHeightFactor: 2})
 
-       doc.text(`Abidjan,${currentDate} `,  150,210);
+       doc.text(`Abidjan,${currentDate} `,  120,210);
 
-       
-       doc.text(`Le Medecin `,  150,220);
+       doc.setFont("arial", "bold");
+       doc.line(150, 222, 150,222);
+       doc.text(`Signature et cachet du medecin `,  120,220);
 
 
         return doc;
--- src/app/shared/api/service/gmhis.shared.DocPdf.service.ts
+++ src/app/shared/api/service/gmhis.shared.DocPdf.service.ts
@@ -5,21 +5,15 @@ import { UserService } from "src/app/_services";
 
 @Injectable({providedIn: 'root'})
 export class GMHISSharedDocPdfService  {
-    
-    private user: User;
+
     constructor(private userService: UserService){}
 
-
-
     public docHeader(titleValue:string, titleX:number): jsPDF {
-        this.user = this.userService.getUserFromLocalCache();
-
+        let user = this.userService.getUserFromLocalCache();
 
         var doc = new jsPDF();
-
         doc.setFillColor(230, 230, 230);
-        doc.addImage(this.user.facility.logo, "JPEG", 20, 10, 30, 30);
-
+        doc.addImage(user.facility.logo, "JPEG", 20, 10, 30, 30);
         doc.setFont("arial", "bold");
         doc.setFontSize(21);
         doc.rect(20, 50, 172, 15)
